a369d1cfd76c7051d71465f27481ece31b360ca5
Sherif-Moheep <sherifmohep@gmail.com>
2025-11-02 23:29:04 +0200

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.protolite.well.known.types)
+    implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.ui.graphics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
