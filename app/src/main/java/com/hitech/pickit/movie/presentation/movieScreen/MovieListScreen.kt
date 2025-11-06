a4cbda0ea73bd66474a8bd410a5aa91a0da43dd5
Robert Romany <ropertromany001@gmail.com>
2025-11-06 13:38:43 +0200

    API & MovieScreen

--- app/src/main/java/com/hitech/pickit/movie/presentation/movieScreen/MovieListScreen.kt
+++ app/src/main/java/com/hitech/pickit/movie/presentation/movieScreen/MovieListScreen.kt
@@ -1,43 +1,25 @@
 package com.hitech.pickit.movie.presentation.movieScreen
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import coil.compose.AsyncImage
-import com.hitech.pickit.movie.presentation.models.MovieUi
 import com.hitech.pickit.movie.presentation.models.toMovieUi
 import com.hitech.pickit.movie.presentation.movieScreen.components.MovieItem
 import com.hitech.pickit.movie.presentation.movie_fav_list.DiscoverMoviesViewModel
-
 
 
 @Composable
@@ -73,7 +55,7 @@ fun MovieListScreen(
 
         uiState.error != null -> {
             Text(
-                text = "Error: ${uiState.error.toString()}",
+                text = "Error: ${uiState.error}",
                 modifier = Modifier.padding(16.dp),
                 textAlign = TextAlign.Center,
                 color = Color.Red
--- app/src/main/java/com/hitech/pickit/movie/presentation/movie_fav_list/MovieFavListScreen.kt
+++ app/src/main/java/com/hitech/pickit/movie/presentation/movie_fav_list/MovieFavListScreen.kt
@@ -4,146 +4,121 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import coil.compose.AsyncImage
 import com.hitech.pickit.R
-import com.hitech.pickit.movie.domain.Movie
-import com.hitech.pickit.movie.presentation.models.MovieUi
-import com.hitech.pickit.movie.presentation.models.toMovieUi
-import com.hitech.pickit.movie.presentation.movie_fav_list.DiscoverMoviesViewModel
 import com.hitech.pickit.movie.presentation.movie_fav_list.components.MovieListItem
 import com.hitech.pickit.movie.presentation.movie_fav_list.components.MoviePreview
 import com.hitech.pickit.movie.utili.MovieState
 import com.hitech.pickit.ui.theme.PickItTheme
-import dagger.hilt.android.lifecycle.HiltViewModel
 
 
-//@Composable
-//fun MovieListScreen(
-//    state: MovieState,
-//    modifier: Modifier = Modifier
-//) {
-//    
-//    val configuration = LocalConfiguration.current
-//    val screenHeight = configuration.screenHeightDp.dp
-//    val screenWidth = configuration.screenWidthDp.dp
-//    if (state.isLoading) {
-//        Box(
-//            modifier = modifier
-//                .fillMaxSize()
-//                .background(MaterialTheme.colorScheme.surface),
-//            contentAlignment = Alignment.Center
-//        ) {
-//            //shimmer
-//            CircularProgressIndicator()
-//        }
-//
-//    } else {
-//        Box(
-//            modifier = modifier
-//                .fillMaxSize()
-//                .background(MaterialTheme.colorScheme.surface),
-//            contentAlignment = Alignment.Center
-//        ) {
-//
-//            Image(
-//                modifier = Modifier.fillMaxSize(),
-//                painter = painterResource(R.drawable.joker),
-//                contentDescription = "",
-//                contentScale = ContentScale.Crop, alignment = Alignment.TopCenter
-//            )
-//            Box(
-//                modifier = Modifier
-//                    .matchParentSize()
-//                    .background(
-//                        Brush.verticalGradient(
-//                            colors = listOf(
-//                                Color.Transparent,
-//                                MaterialTheme.colorScheme.surface
-//                            ), startY = 300f,
-//                            endY = 1200f
-//                        )
-//                    )
-//            )
-//            LazyRow(
-//                modifier = Modifier,
-//                horizontalArrangement = Arrangement.Center,
-//                verticalAlignment = Alignment.Bottom
-//            ) {
-//                items(
-//                    state.movies,
-//                    key = { it.id }
-//                ) { movie ->
-//                    MovieListItem(
-//                        movieUi = movie,
-//                        onclick = {},
-//                        screenHeight = screenHeight,
-//                        modifier = Modifier
-//                            .padding(start = 16.dp, top = 50.dp)
-//                            .height(screenHeight * 0.9f)
-//                            .width(screenWidth * 0.9f)
-//                    )
-//                }
-//            }
-//        }
-//
-//
-//    }
-//
-//}
-//
-//
-//@Preview
-//@Composable
-//private fun MovieFavListScreenPreview() {
-//    PickItTheme {
-//        MovieListScreen(
-//            state = MovieState(
-//                movies = (1..5).map {
-//                    MoviePreview.copy(id = it.toString())
-//                }
-//
-//            )
-//        )
-//    }
-//
-//}
+@Composable
+fun MovieListScreen(
+    state: MovieState,
+    modifier: Modifier = Modifier
+) {
+
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+    if (state.isLoading) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface),
+            contentAlignment = Alignment.Center
+        ) {
+            //shimmer
+            CircularProgressIndicator()
+        }
+
+    } else {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(R.drawable.joker),
+                contentDescription = "",
+                contentScale = ContentScale.Crop, alignment = Alignment.TopCenter
+            )
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                MaterialTheme.colorScheme.surface
+                            ), startY = 300f,
+                            endY = 1200f
+                        )
+                    )
+            )
+            LazyRow(
+                modifier = Modifier,
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.Bottom
+            ) {
+                items(
+                    state.movies,
+                    key = { it.id }
+                ) { movie ->
+                    MovieListItem(
+                        movieUi = movie,
+                        onclick = {},
+                        screenHeight = screenHeight,
+                        modifier = Modifier
+                            .padding(start = 16.dp, top = 50.dp)
+                            .height(screenHeight * 0.9f)
+                            .width(screenWidth * 0.9f)
+                    )
+                }
+            }
+        }
+
+
+    }
+
+}
+
+
+@Preview
+@Composable
+private fun MovieFavListScreenPreview() {
+    PickItTheme {
+        MovieListScreen(
+            state = MovieState(
+                movies = (1..5).map {
+                    MoviePreview.copy(id = it.toString())
+                }
+
+            )
+        )
+    }
+
+}
 
 
 
