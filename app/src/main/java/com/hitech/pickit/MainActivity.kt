7c3236f2a990739b09772de63b73a235c675a0eb
Eng_Osama <ugs.16230085@eng.suez.edu.eg>
2025-11-11 19:37:07 +0200

    Added splash and onboarding screens and APIs

--- app/src/main/java/com/hitech/pickit/MainActivity.kt
+++ app/src/main/java/com/hitech/pickit/MainActivity.kt
@@ -3,25 +3,59 @@ package com.hitech.pickit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.hitech.pickit.movie.presentation.movie_fav_list.components.MoviePreview
-import com.hitech.pickit.ui.theme.PickItTheme
-import com.robert.pickit.movie.presentation.movie_list.MovieListScreen
-import com.robert.pickit.movie.presentation.movie_list.MovieState
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hitech.pickit.ui.theme.OnBoardingPage
+import com.hitech.pickit.ui.theme.OnboardingScreen
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PickItTheme {
-
-            }
+            MyApp()
         }
     }
 }
 
+@Composable
+fun MyApp() {
+    var showSplash by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        kotlinx.coroutines.delay(2000)
+        showSplash = false
+    }
+
+    if (showSplash) {
+        SplashScreen()
+    } else {
+        OnboardingScreen(onFinish = {
+           // h3mel intent yro7 3la elhome
+        })
+    }
+}
+
+@Composable
+fun SplashScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                text = "PickIt",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+    }
+}
