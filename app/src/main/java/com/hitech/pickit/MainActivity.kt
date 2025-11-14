a711c8f95a04a9659a139351d68a08dfba472817
Robert Romany <ropertromany001@gmail.com>
2025-11-14 19:45:56 +0200

    Update (Robert)

--- app/src/main/java/com/hitech/pickit/MainActivity.kt
+++ app/src/main/java/com/hitech/pickit/MainActivity.kt
@@ -3,19 +3,20 @@ package com.hitech.pickit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.hitech.pickit.ui.theme.OnBoardingPage
 import com.hitech.pickit.ui.theme.OnboardingScreen
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +40,7 @@ fun MyApp() {
         SplashScreen()
     } else {
         OnboardingScreen(onFinish = {
-           // h3mel intent yro7 3la elhome
+            // h3mel intent yro7 3la elhome
         })
     }
 }
