8fbf0e27ff28ee26ead17919a6b6652b483a46fe
Robert Romany <ropertromany001@gmail.com>
2025-11-14 21:34:43 +0200

    Update OnBoardingPage.kt & onBoardingScreensUI.kt (Robert)

--- app/src/main/java/com/hitech/pickit/MainActivity.kt
+++ app/src/main/java/com/hitech/pickit/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -19,8 +20,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.hitech.pickit.movie.presentation.movieScreen.MovieListRoute
+import com.hitech.pickit.movie.presentation.onBoardingScreen.OnboardingScreen
 import com.hitech.pickit.ui.theme.PickItTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/hitech/pickit/ui/theme/OnBoardingPage.kt
+++
@@ -1,7 +0,0 @@
-package com.hitech.pickit.ui.theme
-
-data class OnBoardingPage(
-    val title: String,
-    val description: String,
-    val imageRes: Int
-)
